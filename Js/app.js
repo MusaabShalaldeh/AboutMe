abbe3f098d2a4a976bb7716ae202ae9300a47535
MusaabShalaldeh <musaabsh65@yahoo.com>
2021-06-22 12:45:10 +0300

    fixed code

--- Js/app.js
+++ Js/app.js
@@ -1,53 +1,52 @@
 'use strict'
-let userName;
+let userName; //Hello random Person on the internet, what is your name ?
 let userScore = 0;
 
 
-let question1;
-let question2;
-let question3;
-let question4;
-let question5;
-let question6;
+let question1; //is the person who made this page Musa'b ?
+let question2; //is Musa'b's a Game Developer aswell ?
+let question3; //Musa'b likes to listen to music while working, is that correct ?
+let question4; //is Musa'b currently living in Amman, Jordan ?
+let question5; //does Musa'b hate doing reads ?
 
 
 function startAskingQuestions()
 {
     userName = prompt("Hello random Person on the internet, what is your name ?");
 
+    while(!userName)
+    {
+        userName = prompt("Please enter your name!");
+    }
+
     alert("Welcome "+userName+"!");
+    alert("please answer the upcoming questions with yes/no or you will hurt yourscore!");
 
-    question1 = confirm("is the person who made this page Musa'b ?");
-    if(question1)
-        userScore++;
+    question1 = prompt("is the person who made this page Musa'b ?");
+    CheckAnswer(question1);
+    console.log(question1);
 
-    question2 = confirm("is Musa'b's a Game Developer aswell ?");
-    if(question2)
-        userScore++;
+    question2 = prompt("is Musa'b's a Game Developer aswell ?");
+    CheckAnswer(question2);
+    console.log(question2);
 
-    question3 = confirm("is Musa'b a Graphic Designer ?");
-    if(!question3)
-        userScore++;
+    question3 = prompt("Musa'b likes to listen to music while working, is that correct ?");
+    CheckAnswer(question3);
+    console.log(question3);
 
-    question4 = confirm("Musa'b likes to listen to music while working, is that correct ?");
-    if(question4)
-        userScore++;
+    question4 = prompt("is Musa'b currently living in Amman, Jordan ?");
+    CheckAnswer(question4);
+    console.log(question4);
 
-    question5 = confirm("is Musa'b currently living in Amman, Jordan ?");
-    if(question5)
-        userScore++;
-
-    question6 = confirm("does Musa'b hate doing reads?");
-    if(question6)
-        userScore++;
+    question5 = prompt("does Musa'b hate doing reads ?");
+    CheckAnswer(question5);
+    console.log(question5);
 
 
     //give result to user
 
     GiveResultToUser();
 }
-
-
 function GiveResultToUser()
 {
 
@@ -56,10 +55,35 @@ function GiveResultToUser()
     {
         alert("I am really glad to meet you, "+userName+", now you know a bit more about me!");
     }
+    else if(userScore <= 4 && userScore >= 2)
+    {
+        alert("you answered few questions correctly, but some are not correct :/")
+    }
     else
     {
         alert("were you trying to put wrong answers on purpose, "+userName+" ?, please read again!");
     }
 
+    alert("you score is "+userScore+"/5");
+
     userScore = 0;
 }
+
+function CheckAnswer(value)
+{
+    switch(value.toLowerCase()){
+        case 'yes':
+            userScore++;
+            break;
+        case 'no':
+            userScore--;
+            break;
+        default:
+            alert('please answer with yes or no...decreasing your score..');
+            userScore--;
+            break;
+    }
+        
+}
+
+
